45d80570ad165ee3158ff26c15ef2fd5f416ffed
manana <manana@Core11-41>
2018-01-18 12:28:37 +0100

    version 1 - BBDD Proyecto

--- 01_ejemplo_bd/src/principal/AltaContactos.java
+++ 01_ejemplo_bd/src/principal/AltaContactos.java
@@ -2,16 +2,25 @@ package principal;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AltaContactos {
 
 	public static void main(String[] args) {
-		
 		//creamos conexion a la BBDD
-		Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda", "root", "root");
-		String sql = "Insert into contactos(nombre,email,telefono) values('eclipse','eclipse@gmail.com',3333)";
-		
-
+		Connection cn;
+		try {
+			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda", "root", "root");
+			String sql = "Insert into contactos(nombre,email,telefono) values('eclipse','eclipse@gmail.com',3333)";
+			//creamos objeto Statement y enviamos instruccion SQL
+			Statement st=cn.createStatement();
+			st.execute(sql);
+			cn.close();
+			System.out.println("Contacto añadido.");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
