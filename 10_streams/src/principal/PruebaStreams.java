b4f1df025dfa4e699a08f88815b6543df9deaf07
Andres <andres.bravo.sanz@gmail.com>
2018-01-23 22:41:45 +0100

    Streams prueba peek

--- 10_streams/src/principal/PruebaStreams.java
+++ 10_streams/src/principal/PruebaStreams.java
@@ -1,5 +1,6 @@
 package principal;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class PruebaStreams {
@@ -15,9 +16,22 @@ public class PruebaStreams {
 		Stream<String> nombres=Stream.of("Juanito","Jose","Luis","Angel","Helena");
 		nombres.filter(s->s.length()>5).map(n->n.toUpperCase()).forEach(n->System.out.println(n));
 		
-		//peek, operaciones pendientes, en modo lazy cuando se ejecute la operación final.
-		numeros.distinct().peek(s->System.out.println(s)).count();
-		//hay algún numero par mayor que 10?
+		//Prueba peek
+		System.out.println("Prueba metodo peek");
+		Stream<Integer> numeros3=Stream.of(4,8,2,43,3,8,9,0,2,6,7);
+		//peek, operaciones pendientes, en modo lazy cuando se ejecute la operaciï¿½n final.
+		numeros3.distinct().peek(s->System.out.println("Impresion peek: " + s)).filter(s->s<=4).forEach(s->System.out.println("Impresion final"+s));
+		
+		//Ejemplo peek
+		System.out.println("Otra prueba peek");
+		Stream.of("one", "two", "three", "four")
+        .filter(e -> e.length() > 3)
+        .peek(e -> System.out.println("Filtered value: " + e))
+        .map(String::toUpperCase)
+        .peek(e -> System.out.println("Mapped value: " + e))
+        .collect(Collectors.toList());
+
+		//hay algï¿½n numero par mayor que 10?
 		Stream<Integer> numeros2=Stream.of(4,8,2,43,3,8,9,0,2,6,7);
 		System.out.println(numeros2.anyMatch(n->(n>10 && n%2==0)));
 		
