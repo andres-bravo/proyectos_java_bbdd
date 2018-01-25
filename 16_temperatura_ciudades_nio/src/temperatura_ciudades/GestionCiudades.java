788d1882e7c618346dc114c9c4f528352df82660
manana <manana@Core11-41>
2018-01-25 11:18:12 +0100

    Temperatura Ciudades, ficheros nio, streams V1

--- 16_temperatura_ciudades_nio/src/temperatura_ciudades/GestionCiudades.java
+++ 16_temperatura_ciudades_nio/src/temperatura_ciudades/GestionCiudades.java
@@ -6,9 +6,7 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,10 +14,6 @@ import beans.Ciudad;
 
 public class GestionCiudades {
 	String ficherociudades;
-	Path pt;
-	public GestionCiudades() {
-		pt=Paths.get(ficherociudades);
-	}
 	public GestionCiudades(String nombrefichero){
 		this.ficherociudades=nombrefichero;
 	}
@@ -40,11 +34,12 @@ public class GestionCiudades {
 	}
 	public double temperaturaMedia() {
 		double db=0.0;
-		try {
-		db= Files.lines(pt)
-				.mapToDouble(s->Double.parseDouble(s.split("[|]")[1]))
-				.average()
-				.getAsDouble();
+		try(FileReader fr = new FileReader(this.ficherociudades);
+				BufferedReader bf = new BufferedReader(fr)) {
+				db=bf.lines()
+						.mapToDouble(s->Double.parseDouble(s.split("[|]")[1]))
+						.average()
+						.getAsDouble();
 		}
 		catch(IOException e) {
 			e.printStackTrace();
@@ -55,10 +50,11 @@ public class GestionCiudades {
 		Ciudad c = null;
 		String linea = "";
 		//return st.max((a,b)->a.getEdad()-b.getEdad()).get();
-		try {
+		try(FileReader fr = new FileReader(this.ficherociudades);
+				BufferedReader bf = new BufferedReader(fr)) {
 				//El comparator metodo compare devuelve un entero por lo que perdería precision puedo hacerlo convirtiendo
 				//la salida del comparator a int pero pierdo precision
-				linea=Files.lines(pt).max((a,b)-> (int)(Double.parseDouble(a.split("[|]")[1]) - Double.parseDouble(b.split("[|]")[1])))
+				linea=bf.lines().max((a,b)-> (int)(Double.parseDouble(a.split("[|]")[1]) - Double.parseDouble(b.split("[|]")[1])))
 						.get();
 				//Se puede hacer con operador ternario que devuelve el primer o segundo elemento.
 				c = new Ciudad(linea.split("[|]")[0],Double.parseDouble(linea.split("[|]")[1]));
@@ -71,8 +67,9 @@ public class GestionCiudades {
 	}		
 	public List<Ciudad> recuperarTodas(){
 		List<Ciudad>listaciudades = null;
-		try{
-				listaciudades=Files.lines(pt)
+		try(FileReader fr = new FileReader(this.ficherociudades);
+				BufferedReader bf = new BufferedReader(fr)) {
+				listaciudades=bf.lines()
 						.map(s->new Ciudad(s.split("[|]")[0],Double.parseDouble(s.split("[|]")[1])))
 						.collect(Collectors.toList());		
 		}		
