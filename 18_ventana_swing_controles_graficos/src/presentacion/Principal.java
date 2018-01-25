c37d22d11cfe75053f8337bc44970bcc7b857b67
manana <manana@Core11-41>
2018-01-25 13:39:46 +0100

    Controles graficos, swing con codigo V1

---
+++ 18_ventana_swing_controles_graficos/src/presentacion/Principal.java
@@ -0,0 +1,13 @@
+package presentacion;
+
+import presentacion.graficos.JVentana;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		JVentana jv= new JVentana();
+	
+		
+	}
+
+}
---
+++ 18_ventana_swing_controles_graficos/src/presentacion/graficos/JVentana.java
@@ -0,0 +1,38 @@
+package presentacion.graficos;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class JVentana extends JFrame {
+	public JVentana() {
+		//definición de título para la ventana heredado de
+		//constructor de JFrame
+		super("Primera Ventana");
+		//Especificamos que se cierre realmente si se cierra la ventana
+		//para que finalize el programa.
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		//posicionamiento desde posicion 100,100 Ancho y Alto 800,400
+		this.setBounds(100, 100, 800, 400);
+		//realizamos creación y colocacion de 
+		//componentes gráficos
+		iniciarComponentes();
+		this.setVisible(true);
+	}
+	private void iniciarComponentes() {
+		//Por defecto los componentes son mostrados ocupando todo el frame y unos encima de otros
+		//Anulo este comportamiento, podría definir distintos comportamientos, en este caso lo anulo.
+		this.setLayout(null);
+		//Se saca a un metodo ya que tiene muchas instrucciones para mostrar todos los controles
+		//Se le llama en el constructor antes de visualizar la ventana setVisible(true).
+		JButton bt = new JButton("Pulsar");
+		JLabel lb = new JLabel("Mensaje");
+		//Posicionamiento del ojeto
+		bt.setBounds(150, 100, 120, 50);
+		lb.setBounds(300, 50, 120,50);
+		//añadir control a la ventana
+		this.add(bt);
+		this.add(lb);
+			
+	}
+}
